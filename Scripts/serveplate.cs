212034029baf6efd807bc505915bb36169743aac
jglosa <113453123+jglosa@users.noreply.github.com>
2023-05-28 20:59:34 +0900

    add correct/wrong sound

--- Scripts/serveplate.cs
+++ Scripts/serveplate.cs
@@ -7,6 +7,9 @@ public class serveplate : MonoBehaviour
     public int thisPlate;
     GameObject Destroywrong;
 
+    public AudioSource correctAudio;
+    public AudioSource wrongAudio;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,52 +23,70 @@ public class serveplate : MonoBehaviour
         if (gameflow.orderValue[gameflow.plateNum] == gameflow.plateValue[gameflow.plateNum])
         {
             Debug.Log("correct"+ " "+ gameflow.plateNum+" "+gameflow.orderValue[gameflow.plateNum]);
-            gameflow.plateValue[gameflow.plateNum] =0;      
+            gameflow.plateValue[gameflow.plateNum] =0;
+            correctAudio.Stop();
+            correctAudio.Play();
         }
         else
         {
+
             if(gameflow.plateValue[gameflow.plateNum]==110000)
             {
                 Destroywrong= GameObject.Find("Sushi_Shrimp Variant(Clone)");
                 Destroy(Destroywrong, 1f);
+
+                wrongAudio.Stop();
+                wrongAudio.Play();
+
+                gameflow.plateValue[gameflow.plateNum] = 0;
             }
 
             if (gameflow.plateValue[gameflow.plateNum] == 101000)
             {
                 Destroywrong = GameObject.Find("Sushi_Salmon Variant(Clone)");
-                Destroy(Destroywrong, 1f);
+               Destroy(Destroywrong, 1f);
+
+                wrongAudio.Stop();
+                wrongAudio.Play();
+
+                gameflow.plateValue[gameflow.plateNum] = 0;
             }
 
             if (gameflow.plateValue[gameflow.plateNum] == 100100)
             {
                 Destroywrong = GameObject.Find("Sushi_Tuna Variant(Clone)");
                 Destroy(Destroywrong, 1f);
+
+                wrongAudio.Stop();
+                wrongAudio.Play();
+
+                gameflow.plateValue[gameflow.plateNum] = 0;
             }
 
             if (gameflow.plateValue[gameflow.plateNum] == 100010)
             {
                 Destroywrong = GameObject.Find("Sushi_Gizzard_Shad Variant(Clone)");
                 Destroy(Destroywrong, 1f);
+
+                wrongAudio.Stop();
+                wrongAudio.Play();
+
+                gameflow.plateValue[gameflow.plateNum] = 0;
             }
 
             if (gameflow.plateValue[gameflow.plateNum] == 100001)
             {
                 Destroywrong = GameObject.Find("Sushi_Squid Variant(Clone)");
                 Destroy(Destroywrong, 1f);
+
+                wrongAudio.Stop();
+                wrongAudio.Play();
+
+                gameflow.plateValue[gameflow.plateNum] = 0;
             }
 
-            //gameflow.plateValue[gameflow.plateNum] = 0;
         }
 
-        gameflow.emptyPlateNow = transform.position.x;
-        StartCoroutine(platereset());
-        
     }
 
-    IEnumerator platereset()
-    {
-        yield return new WaitForSeconds(.2f);
-        gameflow.emptyPlateNow = -1;
-        gameflow.totalCash += gameflow.orderTimer[thisPlate] * .10f;
-    }
 }
