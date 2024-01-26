e750720b5c4e2252ba5a5d6af6f5650d4e5f5e56
glosa <jumi4339@naver.com>
2024-01-26 10:23:49 +0900

    destroy timing perfect

--- Scripts/Collision_destroy_with_rice.cs
+++ Scripts/Collision_destroy_with_rice.cs
@@ -28,80 +28,54 @@ public class Collision_destroy_with_rice : MonoBehaviour
     void OnCollisionEnter(Collision col)
     {
         DestroyRice = GameObject.Find("Sashimi_rice(Clone)");
-        Destroy(DestroyRice, 3);
 
         if (col.gameObject.name == "Tuna_sashimi(Clone)")
         {
-            Destroy(DestroyRice,0);
-            Destroy(this, 0);
+            Destroy(DestroyRice);
+            Destroy(this);
             Destroy(col.gameObject);
-            Destroy(this, 0);
             gameflow.plateValue[gameflow.plateNum] = (100000+100);
-            Destroy(DestroyRice, 0);
-            Destroy(this, 0);
             GameObject TunaSushi = Instantiate(Tuna, new Vector3((float)-2.3, (float)1.991, gameflow.plateZpos), Tuna.transform.rotation);
-            Destroy(DestroyRice, 0);
-            Destroy(this, 0);
             Destroy(TunaSushi,3);
         }
 
         if (col.gameObject.name == "Ebi_sashimi(Clone)")
         {
-            Destroy(DestroyRice, 0);
-            Destroy(this, 0);
+            Destroy(DestroyRice);
+            Destroy(this);
             Destroy(col.gameObject);
-            Destroy(this, 0);
             gameflow.plateValue[gameflow.plateNum] = (100000 + 10000);
-            Destroy(DestroyRice, 0);
-            Destroy(this, 0);
             GameObject EbiSushi = Instantiate(Ebi, new Vector3((float)-2.3, (float)1.791, gameflow.plateZpos), Ebi.transform.rotation);
-            Destroy(DestroyRice, 0);
-            Destroy(this, 0);
             Destroy(EbiSushi, 3);
         }
 
         if (col.gameObject.name == "Squid_sashimi(Clone)")
         {
-            Destroy(DestroyRice, 0);
-            Destroy(this, 0);
+            Destroy(DestroyRice);
+            Destroy(this);
             Destroy(col.gameObject);
-            Destroy(this, 0);
             gameflow.plateValue[gameflow.plateNum] = (100000 + 1);
-            Destroy(DestroyRice, 0);
-            Destroy(this, 0);
             GameObject SquidSushi =Instantiate(Squid, new Vector3((float)-2.3, (float)1.791, gameflow.plateZpos), Squid.transform.rotation);
-            Destroy(DestroyRice, 0);
-            Destroy(this, 0);
             Destroy(SquidSushi, 3);
         }
 
         if (col.gameObject.name == "Kohada_sashimi(Clone)")
         {
-            Destroy(DestroyRice, 0);
-            Destroy(this, 0);
+            Destroy(DestroyRice);
+            Destroy(this);
             Destroy(col.gameObject);
-            Destroy(this, 0);
             gameflow.plateValue[gameflow.plateNum] = (100000 + 10);
-            Destroy(DestroyRice, 0);
-            Destroy(this, 0);
             GameObject KohadaSushi = Instantiate(Kohada, new Vector3((float)-2.3, (float)1.791, gameflow.plateZpos), Kohada.transform.rotation);
-            Destroy(DestroyRice, 0);
-            Destroy(this, 0);
             Destroy(KohadaSushi, 3);
         }
 
         if (col.gameObject.name == "Salmon_sashimi(Clone)")
         {
-            Destroy(DestroyRice, 0);
-            Destroy(this, 0);
+            Destroy(DestroyRice);
+            Destroy(this);
             Destroy(col.gameObject);
-            Destroy(this, 0);
             gameflow.plateValue[gameflow.plateNum] = (100000 + 1000);
-            Destroy(DestroyRice, 0);
-            Destroy(this, 0);
             GameObject SalmonSushi = Instantiate(Salmon, new Vector3((float)-2.3, (float)1.791, gameflow.plateZpos), Salmon.transform.rotation);
-            Destroy(DestroyRice, 0);
-            Destroy(this, 0);
             Destroy(SalmonSushi, 3);
         }
     }
