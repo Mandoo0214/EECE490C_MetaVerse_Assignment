273a51da000cd9df2886391cd436bd5b705290a2
glosa <jumi4339@naver.com>
2024-01-26 14:04:52 +0900

    sheet texture really change

--- Scripts/Collision_destroy_with_rice.cs
+++ Scripts/Collision_destroy_with_rice.cs
@@ -15,15 +15,11 @@ public class Collision_destroy_with_rice : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
-
-    }
+    {}
 
     // Update is called once per frame
     void Update()
-    {
-
-    }
+    {}
 
     void OnCollisionEnter(Collision col)
     {
